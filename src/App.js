b98801aca52c7d67ebf495359eac31e69caa51e5
Humza-A <humzaaamir61@gmail.com>
2022-07-26 18:22:07 -0400

    Added: Personal info storage (array) Editing: layout style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Personal from "./components/Personal";
+import  "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="layout">
+          <div>
+            <Personal />
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
